5bd2ee1bfc9d668becc3053cb218df9e6a8344d5
Alp Imrek <alp.imrek@adcolony.com>
2022-08-02 17:34:11 +0300

    boilerplate initialized with router and styled component

---
+++ src/components/index.ts
@@ -0,0 +1,2 @@
+export * from "./style";
+export { default as Header } from "./Header";
---
+++ src/components/style.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const AppContainer = styled.div``;
+
+export const PageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const HeaderContainer = styled.div``;
